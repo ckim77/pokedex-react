048d81e23b201a351d40ebd146a38dd1c3119365
Chris Kim <ckjr92@gmail.com>
2022-03-17 00:20:46 -0600

    finished back end, need to add css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import './App.css';
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+// import axios from 'axios';
 
 function App() {
-  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   const [pokemon, setPokemon] = useState('pikachu');
   const [pokemonInfo, setPokemonInfo] = useState([]);
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
+  const pokemonArray = [];
 
-  function handleChange(evt) {
-    setPokemon(evt.target.value.tolower());
+  const getPokemon = async() => {
+    try {
+      const res = await axios.get(url);
+      pokemonArray.push(res.data);
+      setPokemonInfo(pokemonArray);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  useEffect (() => {
+    getPokemon();
+  }, [])
+
+  function handleChange (evt) {
+    setPokemon(evt.target.value.toLowerCase());
+  }
+  
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    getPokemon();
   }
 
   return (
@@ -16,12 +39,28 @@ function App() {
       <h1>Pokedex</h1>
       <form>
         <input 
+          placeholder = "Enter pokemon name"
           type = 'text'
-          placeholder = 'Enter Pokemon name'
           onChange = {handleChange}
         />
 
+        <input
+          type = "submit"
+          onClick = {handleSubmit}
+        />
       </form>
+      {pokemonInfo.map(data => {
+        return (
+          <div>
+            <img src = {data.sprites.front_default} />
+            <h3>{data.types[0].type.name}</h3>
+            <h3>{Math.round(data.height * 3.9)}</h3>
+            <h3>{Math.round(data.weight / 4.3)}</h3>
+            <h3>{data.id}</h3>
+          </div>
+        )
+      })}
+
     </div>
   );
 }
